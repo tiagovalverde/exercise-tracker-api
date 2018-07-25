6071898a9ba3f29f0882e019e2c48ea4b9b970a4
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-07-24 22:33:41 -0400

    Add environment variables

---
+++ app/config/config.js
@@ -0,0 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+console.log(`*** Env: ${env} **`);
+
+if(env === 'development' || env === 'test') {
+    // requires parse json file auto
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    // get json key names as array
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+}
+
